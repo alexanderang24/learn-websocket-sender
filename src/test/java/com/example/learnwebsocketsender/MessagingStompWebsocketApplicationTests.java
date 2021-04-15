68e06ad51355a13d1bf854234814ca07668c9732
Alexander Ang <alexanderang.24@gmail.com>
2021-04-15 22:13:47 +0800

    Initial commit

---
+++ src/test/java/com/example/learnwebsocketsender/MessagingStompWebsocketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.learnwebsocketsender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessagingStompWebsocketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
