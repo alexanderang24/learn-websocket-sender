7af026c23f51530d8d7e189a570f838c4bf0422a
Alexander Ang <alexanderang.24@gmail.com>
2021-04-15 22:23:05 +0800

    Rename main class

--- src/main/java/com/example/learnwebsocketsender/LearnWebsocketSenderApplication.java
+++ src/main/java/com/example/learnwebsocketsender/LearnWebsocketSenderApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MessagingStompWebsocketApplication {
+public class LearnWebsocketSenderApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MessagingStompWebsocketApplication.class, args);
+		SpringApplication.run(LearnWebsocketSenderApplication.class, args);
 	}
 
 }
--- src/test/java/com/example/learnwebsocketsender/LearnWebsocketSenderApplicationTests.java
+++ src/test/java/com/example/learnwebsocketsender/LearnWebsocketSenderApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class MessagingStompWebsocketApplicationTests {
+class LearnWebsocketSenderApplicationTests {
 
 	@Test
 	void contextLoads() {
